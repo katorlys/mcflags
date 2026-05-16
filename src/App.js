ec78781683ae7911c7b186ad4c8024a17f7fb16b
katorly <i@katorly.com>
2026-05-16 22:10:11 +0800

    feat(init): init project with Vite, React, TypeScript, Tailwind

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+function App() {
+    return _jsx("div", { className: "min-h-screen bg-background text-foreground", children: "mcflags" });
+}
+export default App;
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="min-h-screen bg-background text-foreground">mcflags</div>
+}
+
+export default App
